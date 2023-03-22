60b5f782f7cf174f4ec0253c083577eaceed727f
Anubhav Saha <anubhavvsaha2001@gmail.com>
2023-03-23 00:26:42 +0100

    :lipstick: added fonts and icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,37 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import TungstenFont from "@next/font/local";
+
+const tungsten = TungstenFont({
+  src: [
+    {
+      path: "../../public/fonts/Tungsten-Bold.otf",
+      weight: "700",
+    },
+    {
+      path: "../../public/fonts/Tungsten-Semibold.otf",
+      weight: "600",
+    },
+    {
+      path: "../../public/fonts/Tungsten-Medium.otf",
+      weight: "500",
+    },
+    {
+      path: "../../public/fonts/Tungsten-Book.otf",
+      weight: "400",
+    },
+    {
+      path: "../../public/fonts/Tungsten-Light.otf",
+      weight: "300",
+    },
+  ],
+  variable: "--font-tungsten",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${tungsten.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
