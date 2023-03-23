e8b8cfa7be8fe046f16fedf3892a2eb924451049
Anubhav Saha <anubhavvsaha2001@gmail.com>
2023-03-23 19:12:57 +0100

    :arrow_up: updated next/font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import TungstenFont from "@next/font/local";
+import TungstenFont from "next/font/local";
 
 const tungsten = TungstenFont({
   src: [
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import Header from "@/components/header";
 import Sidebar from "@/components/sidebar";
